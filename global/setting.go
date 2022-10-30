6109268d14b2af277b9dabf268df9867a9f05cf9
mneumi <mneumi@outlook.com>
2022-10-31 00:41:05 +0800

    feat: 集成 viper 配置

---
+++ global/setting.go
@@ -0,0 +1,9 @@
+package global
+
+import "github.com/mneumi/gin-boilerplate/pkg/setting"
+
+var (
+	ServerSetting   *setting.ServerSettingS
+	AppSetting      *setting.AppSettingS
+	DatabaseSetting *setting.DatabaseSettingS
+)
